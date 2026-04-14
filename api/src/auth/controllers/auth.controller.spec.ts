e0c4a7882dc98aa1f09d2b23bbdf83531c22873c
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2026-04-14 22:57:31 +0200

    :bug: (api) provide AuthService mock in controller spec

--- api/src/auth/controllers/auth.controller.spec.ts
+++ api/src/auth/controllers/auth.controller.spec.ts
@@ -1,5 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from '@auth/services/auth.service';
+
+const mockAuthService = {
+  login: jest.fn(),
+  signUp: jest.fn(),
+  refresh: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +14,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
