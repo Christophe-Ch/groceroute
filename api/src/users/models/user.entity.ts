ad5551f7d30027280e3b6974ee11f1b3425544c6
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2026-04-16 13:20:08 +0200

    :recycle: (api) Migrate user ID to UUID

--- api/src/users/models/user.entity.ts
+++ api/src/users/models/user.entity.ts
@@ -3,9 +3,9 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   @Exclude()
-  id: number;
+  id: string;
 
   @Column({ unique: true })
   email: string;
