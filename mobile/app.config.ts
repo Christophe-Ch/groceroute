1b1bd279a7af1601b367202020fda76fe4b820f0
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2026-08-27 20:24:39 +0200

    🔧 (mobile) EAS setup

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,7 +4,11 @@ export default ({ config }: ConfigContext) => {
   return {
     ...config,
     extra: {
+      ...config.extra,
       API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL ?? "",
+      eas: {
+        projectId: "f3475bda-dd0e-4fad-a568-2f768143d19f",
+      },
     },
   } as ExpoConfig;
 };
